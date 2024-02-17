48d358474041f15dc69a9df13b7b5e4a64f390c2
Marimba369 <leonelfigueiracaetano@gmail.com>
2024-02-17 10:59:21 +0000

    func: mostrar bandeiras

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Country from './componets/country'
+
+function App() {
+
+  return (
+    <div>
+        <Country/>
+    </div>
+  )
+}
+
+export default App
